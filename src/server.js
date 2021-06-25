79659feada0191e83b770c0e32741b7fe8e80472
LucasCruz <Lucas2002cruz@gmail.com>
2021-06-24 23:52:45 -0400

    Atualização de conteúdo, commit modal funcional

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const route = require('./route')
+const path = require('path')
+
 
 const server = express()
 
-server.set('view.engine', 'ejs ')
+server.set('view engine', 'ejs')
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.static("public"))
 
 server.use(route)
 
