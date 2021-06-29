978449f6f2ca472e5960c674809332d8253759e4
LucasCruz <Lucas2002cruz@gmail.com>
2021-06-29 14:46:03 -0400

    Rotas funcionando

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -22,9 +22,9 @@ function handleClick(event, check = true){
     event.preventDefault()
     const text = check ? "Marcar como lida" : "Excluir"
     const slug = check ? "check" : "delete"
+
     const roomId = document.querySelector("#room-id").dataset.id
     const questionId = event.target.dataset.id
-
     const form = document.querySelector(".modal form")
     form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
 
@@ -35,7 +35,7 @@ function handleClick(event, check = true){
     modal.open()
 }
 
-console.log(roomId)
+
 
 
 
